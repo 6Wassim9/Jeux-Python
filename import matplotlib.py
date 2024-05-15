467df234b10f662c72d4791d770d6a62d9b1ab8e
Tarekov05 <tarik.zeggoud@etu.univ-grenoble-alpes.fr>
2024-05-15 15:17:37 +0200

    Add files via upload

---
+++ import matplotlib.py
@@ -0,0 +1,31 @@
+import pygame # type: ignore
+pygame.init()
+
+#generer la fenetre de notre jeu
+
+pygame.display.set_caption("the fucking maze")
+screen = pygame.display.set_mode((1000, 700))
+
+running = True
+
+#boucle en tant que cette condition est vraie 
+background = pygame.image.load('assets/god-of-war-iii-remastered-1440418809.jpg')
+while running :
+    #appliquer l'arriere plan de notre jeu
+    screen.blit(background, (0,0))
+
+    #mettre a jour l'ecran
+    pygame.display.flip()
+    #si le joueur ferme cette fenetre
+
+    for event in pygame.event.get() :
+
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("Fermeture du jeu")
+
+
+
+
+
