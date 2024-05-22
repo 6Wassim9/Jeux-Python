3085854b6c5471d57d9b5ea62787f59f4c18d61b
Malick72 <ouattaramalick942@gmail.com>
2024-05-22 14:49:56 +0200

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+from player import Player
+from monster import Monster
+import game
+
+#creer une seconde classe qui va representer notre jeu
+class Game :
+
+    def _init_(self) :
+       #generer notre joueur
+       self.all_players =personnage 
+       self.player =Player(self) 
+       self.all_players.add(self.player) 
