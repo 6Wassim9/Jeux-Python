e6916a454c772e304a308091ef9820ba702d25af
Tarekov05 <tarik.zeggoud@etu.univ-grenoble-alpes.fr>
2024-05-26 19:47:02 +0200

    TEST

--- Projet Logiciel/projectile.py
+++ Projet Logiciel/projectile.py
@@ -7,6 +7,6 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.velocity = 5
-        self.image = pygame.image.load('images/zdaegdaz.png')
+        self.image = pygame.image.load('images/pngtree-projectile-explosion-icon-in-flat-circle-isolated-on-white-background-vector-illustration-for-web-picture-image_8063331.png')
         self.rect = self.image.get_rect() 
         
