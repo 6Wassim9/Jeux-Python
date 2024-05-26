55a6ab97020637f90514da796f419b33e1d70783
Tarekov05 <tarik.zeggoud@etu.univ-grenoble-alpes.fr>
2024-05-26 20:36:30 +0200

    TEST

--- Projet Logiciel/projectile.py
+++ Projet Logiciel/projectile.py
@@ -12,5 +12,18 @@ class Projectile(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (50,50))
         self.rect.x = player.rect.x + 120
         self.rect.y = player.rect.y + 80
+        self.origine_image = self.image
+        self.angle = 0
+
+    def rotate(self):
+        #tourner le projectile
+        self.angle += 12
+        self.image = pygame.transform.rotozoom(self.origin_image, self.angle, 1)
     def move (self):
         self.rect.x += self.velocity
+        self.rotate()
+        #verifier si notre projectile n'est plus présent sur l'écran
+        if self.rect.x > 1000:
+
+            #supprimer le projectile (en dehors de l'ecran)
+            self.player.all_projectiles.remove(self)
