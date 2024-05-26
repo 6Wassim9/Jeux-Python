5f0f7414079a70fdf99ce0a756b0064195acd1c8
Tarekov05 <tarik.zeggoud@etu.univ-grenoble-alpes.fr>
2024-05-26 19:34:36 +0200

    TEST

--- Projet Logiciel/player.py
+++ Projet Logiciel/player.py
@@ -4,8 +4,9 @@ class Player(pygame.sprite.Sprite):
     
     def __init__(self):
         super().__init__()
-        self.health = 100 # pa concerné
-        self.max_health = 100 # pas concerné 
+        self.health = 100 
+        self.max_health = 100 
+        self.attack = 10 
         self.velocity = 1 # la vitesse de l'avatar
         self.image = pygame.image.load('images/personnage.png')
         self.newPersonnage = pygame.transform.scale(self.image, (80, 80))
